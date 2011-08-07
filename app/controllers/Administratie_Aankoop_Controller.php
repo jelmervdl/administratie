7bccbf9bb2a2f164b9499599138098001ad43f11
Jelmer <jelmer@ikhoefgeen.nl>
2011-08-07 16:28:01 +0200

    Added totals to factuur details

--- app/controllers/Administratie_Aankoop_Controller.php
+++ app/controllers/Administratie_Aankoop_Controller.php
@@ -18,7 +18,7 @@ class Administratie_Aankoop_Controller extends IHG_Controller_Abstract
 			->set_walker($walker)
 			//->add_column('bedrijf', 'Bedrijf', array($this, '_format_bedrijf'))
 			->add_column('beschrijving', 'Beschrijving', new IHG_Formatter_Rich())
-			->add_column('aantal', 'Aantal', function($aantal) { return number_format($aantal, 1); })
-			->add_column('prijs', 'Prijs', function($prijs) { return '&euro;' . number_format($prijs, 2, ',', '.'); });
+			->add_column('aantal', 'Aantal', function($aantal) { return number_format($aantal, 1); }, 'array_sum')
+			->add_column('prijs', 'Prijs', function($prijs) { return '&euro;' . number_format($prijs, 2, ',', '.'); }, 'array_sum');
 	}
 }
